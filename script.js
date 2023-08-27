4e8d01b115f133ec5f692f8dc9e64df0ad1871a9
harshrupendrasingh <76151564+harshrupendrasingh@users.noreply.github.com>
2023-08-27 20:18:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ==================== typing animatiom =============== */
+var typed = new Typed(".typing",{
+    strings:["","web Developer","DSA Solver"],
+    typeSpeed:100,
+    backspeed:60,
+    loop:true
+})
